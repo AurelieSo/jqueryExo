d7d18e1d0cb326ef873c07afef2c9c90967a07e1
AurelieSo <soucek.aurelie@mgail.com>
2020-11-06 14:00:55 +0100

    exercice 4 fait

---
+++ Exercice 4/assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#firstText').remove();
+    $('#secondtext').empty();
+})
+
+// La méthode remove () supprime les éléments inclus, y compris tout le texte et les nœuds enfants.
+// Cette méthode supprime également les données et les événements des éléments sélectionnés.
+// Conseil: pour supprimer les éléments sans supprimer les données et les événements, utilisez plutôt la méthode detach () .
+
+// La méthode empty () supprime tous les nœuds enfants et le contenu des éléments inclus.
+// Remarque: cette méthode ne supprime pas lui-même ni ses attributs.
+// Conseil: pour supprimer les éléments sans supprimer les données et les événements, utilisez la méthode detach () .

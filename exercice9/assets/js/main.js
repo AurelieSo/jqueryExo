31e32c9028d906239ef1075d475bc00f2ea5ac00
AurelieSo <soucek.aurelie@mgail.com>
2020-11-09 17:05:52 +0100

    modif exercice9

--- exercice9/assets/js/main.js
+++ exercice9/assets/js/main.js
@@ -1,44 +1,54 @@
+// debut de ma version
+// $(document).ready(function(){
 
-$(document).ready(function(){
-
-    $("#green").click(function(){
+//     $("#green").click(function(){
        
-        $("text").children().removeClass().addClass('.colorGreen');
+//         $("text").children().removeClass().addClass('.colorGreen');
         
-        $("p").css({
-        color : 'green',
-        });
-    });    
+//         $("p").css({
+//         color : 'green',
+//         });
+//     });    
         
-    $("#red").click(function(){
+//     $("#red").click(function(){
 
-        $("text").children().removeClass().addClass('.colorRed');
+//         $("text").children().removeClass().addClass('.colorRed');
                 
-        $("p").css({
-            color : 'red'
-        });
-    });
+//         $("p").css({
+//             color : 'red'
+//         });
+//     });
 
-    $("#blue").click(function(){
+//     $("#blue").click(function(){
 
-            $("text").children().removeClass().addClass('.colorBlue');
+//             $("text").children().removeClass().addClass('.colorBlue');
                         
-            $("p").css({
-                color : 'blue',
-            });
-    });
-});
+//             $("p").css({
+//                 color : 'blue',
+//             });
+//     });
+// });
+
+// fin de ma version
 
 
 
-// toutes les balises dans le ul seront rouges
-// tous les enfants de ul seront rouges
-$(document).ready(function(){
-    $('ul').children().css('color', 'red');
-});
+//  correction version 1
+// $(document).ready(function(){
+//     $('green').click(function() {
+//         $('#texte').css('color', 'green');
+//     });
 
-
-
-
- 
+// })
        
+// correction version2 plus simple
+$(document).ready(function(){
+    $('.color').click(function(){
+        // quand on clic sur un eltm qui a une couleur
+    var divColor=$(this).attr('id');
+        $('#text').css('color', divColor);
+        // la couleur est egale à l'id de la div
+        // l'id comprend la couleur
+    })
+
+})

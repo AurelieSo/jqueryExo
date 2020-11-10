2dded87b7b3e3dfc6ee666c8f16c7205f5de8da7
AurelieSo <soucek.aurelie@mgail.com>
2020-11-10 10:00:39 +0100

    ajout exo 9 et 10

--- exercice10/assets/js/main.js
+++ exercice10/assets/js/main.js
@@ -1,58 +1,68 @@
 
-$(document).ready(function(){
+// ma version beaucoup trop longue
+// $(document).ready(function(){
 
-    $("#green").mouseover(function(){
+//     $("#green").mouseover(function(){
        
-        $("text").children().removeClass().addClass('.colorGreen');
+//         $("text").children().removeClass().addClass('.green');
         
-        $("p").css({
-        color : 'green',
-        });
-    });   
-    $("#green").mouseout(function(){
+//         $("p").css({
+//         color : 'green',
+//         });
+//     });   
+//     $("#green").mouseout(function(){
        
-        $("text").children().removeClass().addClass('.colorGreen');
+//         $("text").children().removeClass().addClass('.green');
         
-        $("p").css({
-        color : 'black',
-        });
-    });  
+//         $("p").css({
+//         color : 'black',
+//         });
+//     });  
         
-    $("#red").mouseover(function(){
+//     $("#red").mouseover(function(){
 
-        $("text").children().removeClass().addClass('.colorRed');
+//         $("text").children().removeClass().addClass('.red');
                 
-        $("p").css({
-            color : 'red'
-        });
-    });
-    $("#red").mouseout(function(){
+//         $("p").css({
+//             color : 'red'
+//         });
+//     });
+//     $("#red").mouseout(function(){
        
-        $("text").children().removeClass().addClass('.colorRed');
+//         $("text").children().removeClass().addClass('.red');
         
-        $("p").css({
-        color : 'black',
-        });
-    });  
-    $("#blue").mouseover(function(){
+//         $("p").css({
+//         color : 'black',
+//         });
+//     });  
+//     $("#blue").mouseover(function(){
 
-            $("text").children().removeClass().addClass('.colorBlue');
+//             $("text").children().removeClass().addClass('.blue');
                         
-            $("p").css({
-                color : 'blue',
-            });
-    });
-    $("#blue").mouseout(function(){
+//             $("p").css({
+//                 color : 'blue',
+//             });
+//     });
+//     $("#blue").mouseout(function(){
 
-        $("text").children().removeClass().addClass('.colorBlue');
+//         $("text").children().removeClass().addClass('.blue');
                     
-        $("p").css({
-            color : 'black',
-        });
-});
-});
+//         $("p").css({
+//             color : 'black',
+//         });
+// });
+// });
 
+// correction
+$(document).ready(function(){
+    $('.color').mouseover(function(){
+        $('#text').css('color', this.id);
+    });
+    $('.color').mouseout(function(){
+        $('#text').css('color', 'black');
+    });
 
+})
 
 
 
--- exercice9/assets/js/main.js
+++ exercice9/assets/js/main.js
@@ -49,6 +49,17 @@ $(document).ready(function(){
         $('#text').css('color', divColor);
         // la couleur est egale à l'id de la div
         // l'id comprend la couleur
+        // il faut créer les ccs couleur
     })
 
+})
+
+// autre version d'écriture en optimisant le code
+// ici le this fait référence a ce qui est devant dans la parenthèse
+// le this.id = ligne 48 = $(this).attr('id');
+// le this est l'élément dans lequel on est actuellement ici color
+$(document).ready(function(){
+    $('.color').click(function(){
+        $('#text').css('color', this.id);
+    })
 })
